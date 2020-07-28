091e1e89a0ec9768e90bdeecb5bf9fc760d61861
sandeshdeshmukh1 <stdeshmukh@mitaoe.ac.in>
2020-07-28 22:04:59 +0530

    Updates

---
+++ precache-manifest.68fc781bd8b359c25fbf43302f81a255.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f05ee0b346726d6ee43ab035fe006cfc",
+    "url": "/notes_app/index.html"
+  },
+  {
+    "revision": "448b9a1ffa1b8c6cbfa2",
+    "url": "/notes_app/static/css/main.23037f1a.chunk.css"
+  },
+  {
+    "revision": "f66504500a6418fcc892",
+    "url": "/notes_app/static/js/2.02512200.chunk.js"
+  },
+  {
+    "revision": "e66d8bfcf01b9d69124b67e3e620fab0",
+    "url": "/notes_app/static/js/2.02512200.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "448b9a1ffa1b8c6cbfa2",
+    "url": "/notes_app/static/js/main.52efc93c.chunk.js"
+  },
+  {
+    "revision": "4554b5da8f3f14eca0fc",
+    "url": "/notes_app/static/js/runtime-main.1cd90781.js"
+  }
+]);
